fdbfaf318d0597ba432aaa2dbd2c31ae11cdd628
Ally Preston <APreston@spartaglobal.com>
2019-07-16 16:59:53 +0100

    completed bread example for functions

--- 05function_bread_factory.py
+++ 05function_bread_factory.py
@@ -24,3 +24,22 @@ print(make_dough('water', 'wheat') == 'dough')
 
 print('calling function bake_bread, expecting bread to be returned')
 print(bake_bread('dough') == 'bread')
+
+print('calling function bake_bread with wrong ingredients, expecting outcome to not be bread')
+print(bake_bread('cat') == 'not bread')
+
+print('calling function bake_bread with wrong ingredients, expecting outcome not to be bread')
+print(bake_bread('human bones') != 'bread')
+
+print('---------------------------------------------------------------------------------------------')
+
+def bread_factory(ingredient1, ingredient2):
+    semi_product = make_dough(ingredient1, ingredient2)
+    product = bake_bread(semi_product)
+    return product
+
+print(bread_factory('water', 'wheat') == 'bread')
+
+print(bread_factory('cement', 'bread') != 'bread')
+
+print(bread_factory('wheat', 'water') == 'bread')
